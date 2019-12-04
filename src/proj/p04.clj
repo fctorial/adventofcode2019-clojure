dba5e40c594075916a0fb2b15b86e12b8650c21b
Sagar Tewari <sagartewari01@gmail.com>
2019-12-04 22:00:30 +0530

    p04 10ms 40ms

--- src/proj/p04.clj
+++ src/proj/p04.clj
@@ -5,23 +5,22 @@
           read-string
           (split (slurp "p04.txt") #"-")))
 
-(def p1 (for [n (apply range ip)
-              :let [digs (loop [res ()
-                                curr n]
-                           (if (= curr 0)
-                             res
-                             (recur (conj res (mod curr 10))
-                                    (unchecked-divide-int curr 10))))
-                    cnt (count digs)
-                    neighs (map vector
-                                (take (dec cnt) digs)
-                                (drop 1 digs))
-                    same-pair? (some
-                                 #(apply = %) neighs)
-                    non-dec? (every?
-                               #(apply <= %) neighs)]
-              :when (and same-pair? non-dec?)]
-          n))
+(defn get-digits [n]
+  (loop [res ()
+         curr n]
+    (if (= curr 0)
+      res
+      (recur (conj res (mod curr 10))
+             (unchecked-divide-int curr 10)))))
+
+(defn join-digits [digs]
+  (loop [res 0
+         digs digs]
+    (if (empty? digs)
+      res
+      (recur (+ (first digs)
+                (* 10 res))
+             (rest digs)))))
 
 (defn make-fours [_digs]
   (let [nil-seq [nil]
@@ -33,20 +32,39 @@
            (drop 2 digs)
            (drop 3 digs)))))
 
-(def p2 (for [n (apply range ip)
-              :let [digs (loop [res ()
-                                curr n]
-                           (if (= curr 0)
-                             res
-                             (recur (conj res (mod curr 10))
-                                    (unchecked-divide-int curr 10))))
-                    fours (make-fours digs)
-                    only-pair? (some
-                                 (fn [[a b c d]]
-                                   (and (not= a b)
-                                        (not= c d)
-                                        (= b c))) fours)
-                    non-dec? (apply <= digs)]
-              :when (and non-dec?
-                         only-pair?)]
-          n))
+(defn make-pairs [coll]
+  (map vector
+       coll
+       (drop 1 coll)))
+
+(defn inc-nums [st end]
+  (if (> st end)
+    nil
+    (let [digs (get-digits st)
+          non-dec? (apply <= digs)]
+      (if non-dec?
+        (lazy-seq (cons digs (inc-nums (inc st) end)))
+        (let [pairs (make-pairs digs)
+              inc-ind (inc (count (take-while (fn [[n1 n2]]
+                                                (<= n1 n2)) pairs)))
+              first-part (take inc-ind digs)
+              nxt-st-digs (concat first-part (repeat (- (count digs) inc-ind) (last first-part)))]
+          (recur (join-digits nxt-st-digs) end))))))
+
+(defn p1 []
+  (count (filter
+           (fn [n]
+             (some #(apply = %)
+                   (make-pairs n)))
+           (apply inc-nums ip))))
+
+(defn p2 []
+  (count (filter
+           (fn [n]
+             (some
+               (fn [[a b c d]]
+                 (and (= b c)
+                      (not= a b)
+                      (not= c d)))
+               (make-fours n)))
+           (apply inc-nums ip))))
