d320c67f78b7d7f26683a32b76550069d62cedfc
Sagar Tewari <sagartewari01@gmail.com>
2019-12-07 16:55:00 +0530

    clean up IntCode

--- src/proj/p05.clj
+++ src/proj/p05.clj
@@ -1,110 +1,78 @@
 (ns proj.p05
   (:require [clojure.string :refer [split split-lines]]))
 
-()
 
-(def insns
-  {1  {:ac   3
-       :code (fn [mem args modes]
-               (let [[a1 a2]
-                     (map
-                       #(if (= %2 \0)
-                          (mem %1) %1)
-                       args modes)
-                     a3 (nth args 2)]
-                 [(assoc mem a3 (+ a1 a2)) nil]))}
-   2  {:ac   3
-       :code (fn [mem args modes]
-               (let [[a1 a2]
-                     (map
-                       #(if (= %2 \0)
-                          (mem %1) %1)
-                       args modes)
-                     a3 (nth args 2)]
-                 [(assoc mem a3 (* a1 a2)) nil]))}
-   3  {:ac   1
-       :code (fn [mem [a] _]
-               (println "inputting 5")
-               [(assoc mem a 5) nil])}
+(def ac {1 3
+         2 3
+         3 1
+         4 1
+         5 2
+         6 2
+         7 3
+         8 3
+         99 0})
 
-   4  {:ac   1
-       :code (fn [mem [a] [m]]
-               (let [a (if (= m \0)
-                         (mem a) a)]
-                 (println a)
-                 [mem nil]))}
-
-   5  {:ac   2
-       :code (fn [mem args modes]
-               (let [[a1 a2]
-                     (map
-                       #(if (= %2 \0)
-                          (mem %1) %1)
-                       args modes)]
-                 (if (zero? a1)
-                   [mem nil]
-                   [mem a2])))}
-
-   6  {:ac   2
-       :code (fn [mem args modes]
-               (let [[a1 a2]
-                     (map
-                       #(if (= %2 \0)
-                          (mem %1) %1)
-                       args modes)]
-                 (if (zero? a1)
-                   [mem a2]
-                   [mem nil])))}
-
-   7  {:ac   3
-       :code (fn [mem args modes]
-               (let [[a1 a2]
-                     (map
-                       #(if (= %2 \0)
-                          (mem %1) %1)
-                       args modes)
-                     a3 (nth args 2)]
-                 [(assoc mem a3 (if (< a1 a2) 1 0))
-                  nil]))}
-
-   8  {:ac   3
-       :code (fn [mem args modes]
-               (let [[a1 a2]
-                     (map
-                       #(if (= %2 \0)
-                          (mem %1) %1)
-                       args modes)
-                     a3 (nth args 2)]
-                 [(assoc mem a3 (if (= a1 a2) 1 0))
-                  nil]))}})
-
-(def fsck {0 \0
-           1 \1})
-
-(defn run [prog]
+(defn run [prog input output]
   (loop [mem prog
          ip 0]
     (let [op_full (mem ip)
           op (mod op_full 100)
-          modes (concat
-            (reverse (str (unchecked-divide-int op_full 100)))
-            (repeat \0))]
-      (if (= op 99)
-        mem
-        (let [insn_entry (insns op)
-              ac (insn_entry :ac)
-              modes (take ac modes)
-              impl (insn_entry :code)
-              nxt_ip (+ ip ac 1)
-              args (subvec mem (inc ip) nxt_ip)
-              [res impl_ip] (impl mem args modes)
-              nxt_ip (or impl_ip
-                         nxt_ip)]
-          (recur res nxt_ip))))))
+          ac (ac op)
+          modes (take ac (concat
+                           (reverse (str (unchecked-divide-int op_full 100)))
+                           (repeat \0)))
+          nxt_ip (+ ip ac 1)
+          args_raw (subvec mem (inc ip) nxt_ip)
+          args (mapv
+                 (fn [a m]
+                   (if (= m \0)
+                     (mem a)
+                     a))
+                 args_raw modes)]
+      (case op
+        1 (recur
+            (assoc mem (args_raw 2) (+ (args 0) (args 1)))
+            nxt_ip)
+        2 (recur
+            (assoc mem (args_raw 2) (* (args 0) (args 1)))
+            nxt_ip)
+        3 (recur
+            (assoc mem (args_raw 0) (input))
+            nxt_ip)
+        4 (do
+            (output (args 0))
+            (recur mem
+                   nxt_ip))
+        5 (recur mem
+                 (if (zero? (args 0))
+                   nxt_ip
+                   (args 1)))
+        6 (recur mem
+                 (if (zero? (args 0))
+                   (args 1)
+                   nxt_ip))
+        7 (recur
+            (assoc mem (args_raw 2)
+                       (if (< (args 0) (args 1))
+                         1 0))
+            nxt_ip)
+        8 (recur
+            (assoc mem (args_raw 2)
+                       (if (= (args 0) (args 1))
+                         1 0))
+            nxt_ip)
+        99 mem))))
 
 (def prog (mapv
             read-string
             (filter #(not (empty? %))
                     (split (slurp "p05.txt") #","))))
 
+(defn p1 []
+  (run prog (fn [] 1) println)
+  nil)
 
+
+(defn p2 []
+  (run prog (fn [] 5) println)
+  nil)
