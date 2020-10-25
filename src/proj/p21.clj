b42749ae71794347a31b300b3acf394622576884
Sagar Tiwari <fctorial@gmail.com>
2020-10-25 09:10:22 +0530

    p22 1

--- src/proj/p21.clj
+++ src/proj/p21.clj
@@ -1,5 +1,6 @@
 (ns proj.p21
   (:require [proj.comp :refer [run read-prog extend-prog]]
+            [proj.utils :refer [zip-colls]]
             [clojure.core.async :refer [>! <! go chan close! poll! <!! >!!]]
             [clojure.string :as str]))
 
@@ -23,11 +24,40 @@
   (let [ip (chan 1024)
         op (printing-chan)
         comp (run prog ip op nil)
-        script (str/join "\n" (concat p ["WALK" ""]))]
+        script (str/join "\n" (concat p [""]))]
     (doseq [c script]
       (>!! ip (int c)))
     (<!! comp)
     nil))
 
+(defn gen-matcher [[reg status :as a]]
+  (if status
+    [(str "NOT " reg " T")
+     (str "NOT T T")
+     (str "AND T J")]
+    [(str "NOT " reg " T")
+     (str "AND T J")]))
+
+(defn trace [a]
+  (println (type a) " => " a)
+  a)
+
+(defn compile-script [ss n]
+  (->> ss
+       (map #(take n (concat % (repeat nil))))
+       (map #(zip-colls [\A \B \C \D \E \F \G \H \I] %))
+       (map #(filter (fn [[_ status]] (not (nil? status))) %))
+       (map #(map gen-matcher %))
+       (map #(apply concat %))
+       (apply concat)
+       ))
+
+(defn try-prog-1 [& p]
+  (apply try-prog (concat p ["WALK"])))
+
+(defn try-prog-2 [& p]
+  (apply try-prog (concat p ["RUN"])))
+
 (defn p1 []
-  (try-prog "NOT C T" "NOT C J" "AND T J" "NOT D T" "NOT T T" "AND T J" "NOT A T" "OR T J"))
+  (try-prog-1 "NOT C J" "NOT D T" "NOT T T" "AND T J" "NOT A T" "OR T J"))
+
---
+++ src/proj/proj/p22.clj
@@ -0,0 +1,50 @@
+(ns proj.proj.p22
+  (:require [proj.utils :refer [cfind]]
+            [clojure.string :as str]))
+
+(def N 10007)
+(def orig (fn [i] i))
+
+(defn dealNew [_]
+  (fn [prev]
+    (fn [i]
+      (- N (prev i) 1))))
+
+(defn cut [n]
+  (fn [prev]
+    (fn [i]
+      (mod (- (prev i) n) N))))
+
+(defn dealInc [n]
+  (fn [prev]
+    (fn [i]
+      (mod (* (prev i) n) N))))
+
+(defn compile [prog]
+  (reduce
+    (fn [res [insn arg]]
+      ((insn arg) res))
+    orig
+    prog))
+
+(defn run [p]
+  (loop [res (vec (repeat 10 -1))
+         left (range 10)]
+    (if (empty? left)
+      res
+      (recur (assoc res (p (first left)) (first left))
+             (rest left)))))
+
+(defn transformer [l]
+  (cond
+    (str/starts-with? l "deal into new stack") [dealNew nil]
+    (str/starts-with? l "cut ") [cut (read-string (subs l 4))]
+    (str/starts-with? l "deal with increment ") [dealInc (read-string (subs l 20))]
+    :default (throw (new IllegalStateException))))
+
+
+
+(def parsed (->> "p22.txt"
+                 slurp
+                 str/split-lines
+                 (mapv transformer)))
