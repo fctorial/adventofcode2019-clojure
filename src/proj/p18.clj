8efbbb10579ae65d2ce23e4e80606ab2a2cfd3f4
Sagar Tiwari <fctorial@gmail.com>
2020-11-05 18:04:54 +0530

    p18 boilerplate

---
+++ src/proj/p18.clj
@@ -0,0 +1,6 @@
+(ns proj.p18
+  (:require [proj.p18_1]
+            [proj.p18_2]))
+
+(def p1 proj.p18_1/p1)
+(def p2 proj.p18_2/p2)
--- src/proj/p22.clj
+++ src/proj/p22.clj
@@ -2,23 +2,23 @@
   (:require [proj.utils :refer [cfind]]
             [clojure.string :as str]))
 
-(def N 10007)
-(def orig (fn [i] i))
+(def N1 10007)
+(def orig (fn [i] (bigint i)))
 
 (defn dealNewRev [_]
   (fn [prev]
     (fn [i]
-      (- N (prev i) 1))))
+      (- N1 (prev i) 1))))
 
 (defn cutRev [n]
   (fn [prev]
     (fn [i]
-      (mod (- (prev i) n) N))))
+      (mod (- (prev i) n) N1))))
 
 (defn dealIncRev [n]
   (fn [prev]
     (fn [i]
-      (mod (* (prev i) n) N))))
+      (mod (* (prev i) n) N1))))
 
 (defn compile [prog]
   (reduce
@@ -27,7 +27,7 @@
     orig
     prog))
 
-(defn run [p]
+(defn runRev [p]
   (loop [res (vec (repeat 10 -1))
          left (range 10)]
     (if (empty? left)
@@ -42,8 +42,6 @@
     (str/starts-with? l "deal with increment ") [dealIncRev (read-string (subs l 20))]
     :default (throw (new IllegalStateException))))
 
-
-
 (def parsedRev (->> "p22.txt"
                     slurp
                     str/split-lines
@@ -52,17 +50,45 @@
 (defn p1 []
   ((compile parsedRev) 2019))
 
+(def N2 119315717514047)
+
 (defn dealNew [_]
   (fn [prev]
     (fn [i]
-      (prev (- N 1 i)))))
+      (prev (- N2 1 i)))))
 
 (defn cut [n]
   (fn [prev]
     (fn [i]
-      (prev (- i n)))))
+      (prev (mod (+ i n) N2)))))
 
 (defn dealInc [n]
   (fn [prev]
     (fn [i]
-      )))
+      (prev (mod (* i (- N2 (mod n N2))) N2)))))
+
+(defn run [p]
+  (mapv #(p %) (range 10)))
+
+(defn transformer [l]
+  (cond
+    (str/starts-with? l "deal into new stack") [dealNew nil]
+    (str/starts-with? l "cut ") [cut (bigint (read-string (subs l 4)))]
+    (str/starts-with? l "deal with increment ") [dealInc (bigint (read-string (subs l 20)))]
+    :default (throw (new IllegalStateException))))
+
+(def parsed (->> "p22.txt"
+                 slurp
+                 str/split-lines
+                 (mapv transformer)))
+
+(def prog2 (compile parsed))
+
+(defn p2 []
+  (loop [seen 0
+         curr 2020]
+    (let [nxt (prog2 curr)]
+      (if (= nxt 2020)
+        seen
+        (recur (inc seen)
+               nxt)))))
